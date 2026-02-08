358ec33de552c139cfc107b98c7c43bcd05e893d
xwilson03 <xanderwilson2014@gmail.com>
2026-02-07 22:23:43 -0600

    initial commit

---
+++ scripts/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+REPO_DIR=$(git rev-parse --show-toplevel)
+REPO_NAME=$(basename $REPO_DIR)
+THIS=$(echo $(realpath $0) | sed "s|$REPO_DIR|$REPO_NAME|g")
+
+echo $THIS
+echo ----------
+
+set -eux
+
+main() {
+
+    SOURCE_DIR=$REPO_DIR
+    BUILD_DIR=$REPO_DIR/.build
+    INSTALL_DIR=$REPO_DIR/.install
+
+    cmake \
+        -G Ninja \
+        -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+        -B "$BUILD_DIR" \
+        "$SOURCE_DIR"
+
+    cmake --build "$BUILD_DIR"
+    cmake --install "$BUILD_DIR"
+
+}
+
+main "$@"
